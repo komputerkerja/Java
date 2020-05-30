42f249848678ebee349b8e7451c210241dcfd455
komputerkerja <komputerkerja100@gmail.com>
2020-05-31 00:19:34 +0700

    mulai JAVA dari awal

--- Java 04 - Memahami program/RonalL.java
+++
@@ -1,8 +0,0 @@
-public class RonalL{
-
-
-	public static void main(String[] args){
-		System.out.println("hallo nama saya ronal");	
-	}
-
-}
--- Java 05 - Organisasi program dengan package/Program dengan Manual/src/com/aksata/Main.java
+++
@@ -1,8 +0,0 @@
-package com.aksata;
-
-public class Main{
-
-	public static void main(String[] args){
-		System.out.println("hello Java");
-	}
-}
